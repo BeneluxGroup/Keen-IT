fd36192ec924875b3859115aa95d949b07d486b9
Yvo Rolefes <yvo@earlyharvest.nl>
2024-02-20 13:58:28 +0100

    Update __manifest__.py

--- website_keen/__manifest__.py
+++ website_keen/__manifest__.py
@@ -6,12 +6,9 @@
    'author': 'Yvo Rolefes - Vroege Oogst',
    'license': '...',
    'depends': ['website'],
-   'data': [
-      # ...
-   ],
    'assets': {
         'web.assets_frontend': [
-            ('prepend', 'website_airproof/static/src/scss/primary_variables.scss'),
+            #('prepend', 'website_airproof/static/src/scss/primary_variables.scss'),
             'website_keen/static/src/js/keen.js',
             'website_keen/static/src/scss/keen.scss',
         ],
