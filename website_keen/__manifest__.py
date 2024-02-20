5fb121937aefba284d7b036b7624624eaff6f778
Yvo Rolefes <yvo@earlyharvest.nl>
2024-02-20 16:45:51 +0100

    Nieuwe navbar basis & Versioning
    - Nieuwe navbar basis
    - Nieuwe template basis
    - Versioning aangepast (correcte semantics)

--- website_keen/__manifest__.py
+++ website_keen/__manifest__.py
@@ -2,15 +2,20 @@
    'name': 'Keen-IT Thema',
    'description': 'Keen thema ontwikkeld door Vroege Oogst',
    'category': 'Website/Theme',
-   'version': '0.1',
+   'version': '0.0.1',
    'author': 'Yvo Rolefes - Vroege Oogst',
    'license': 'LGPL-3',
    'depends': ['website'],
+   'data': [
+       #'views/website_templates.xml',
+       'views/navbar_frontend.xml',
+   ],
    'assets': {
         'web.assets_frontend': [
-            #('prepend', 'website_airproof/static/src/scss/primary_variables.scss'),
+            ('prepend', 'website_airproof/static/src/scss/primary_variables.scss'),
             'website_keen/static/src/js/keen.js',
             'website_keen/static/src/scss/keen.scss',
+            'website_keen/static/src/scss/keen_navbar.scss',
         ],
    },
 }
