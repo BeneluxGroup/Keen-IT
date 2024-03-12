7afe510f7375be0f755e13e32062cb92b8e9090f
Yvo Rolefes <yvo@earlyharvest.nl>
2024-03-12 16:29:32 +0100

    Nieuwe Header & Footer

--- custom-addons/website_keen/__manifest__.py
+++ custom-addons/website_keen/__manifest__.py
@@ -1,19 +1,22 @@
 {
    'name': 'Keen-IT Thema',
    'description': 'Keen thema ontwikkeld door Vroege Oogst',
-   'category': 'Theme',
-   'version': '0.0.1',
+   'category': 'Website/Theme',
+   'version': '0.0.2',
    'author': 'Yvo Rolefes - Vroege Oogst',
    'license': 'LGPL-3',
    'depends': ['website'],
    'data': [
-       'views/navbar_frontend.xml',
+       'views/website_templates.xml',
+       'data/presets.xml',
    ],
    'assets': {
+        'web._assets_primary_variables': [
+            'website_keen/static/src/scss/primary_variables.scss'
+        ],
         'web.assets_frontend': [
-            #('prepend', 'website_airproof/static/src/scss/primary_variables.scss'),
             'website_keen/static/src/js/keen.js',
-            #'website_keen/static/src/scss/keen.scss',
+            'website_keen/static/src/scss/keen.scss',
             'website_keen/static/src/scss/keen_navbar.scss',
         ],
    },
